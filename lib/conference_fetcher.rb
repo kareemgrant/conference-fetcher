64a221933d6e4bdce5c108bc511d932e62462d5f
Kareem Grant <kareem@getuserwise.com>
2013-06-27 08:13:03 -0600

    cleaned up method calls with extend self declaration

--- lib/conference_fetcher.rb
+++ lib/conference_fetcher.rb
@@ -4,36 +4,37 @@ require 'faraday'
 require 'json'
 
 module ConferenceFetcher
+  extend self
 
-  def self.get(conference_slug)
+  def get(conference_slug)
     conference_request(conference_slug)
   end
 
-  def self.get_track(track_id)
+  def get_track(track_id)
     track_request(track_id)
   end
 
 private
 
-  def self.conference_request(conference_slug)
+  def conference_request(conference_slug)
     return nil if !conference_slug
 
     response = connection.get(conferences_uri(conference_slug))
     parse_response(response)
   end
 
-  def self.track_request(track_id)
+  def track_request(track_id)
     return nil if !track_id
 
     response = connection.get(track_uri(track_id))
     parse_response(response)
   end
 
-  def self.connection
+  def connection
     Faraday
   end
 
-  def self.parse_response(response)
+  def parse_response(response)
     if response.status == 200
       JSON.parse(response.body)
     else
@@ -41,15 +42,15 @@ private
     end
   end
 
-  def self.conferences_uri(conference_slug)
+  def conferences_uri(conference_slug)
     "#{base_uri}/conferences/#{conference_slug}"
   end
 
-  def self.track_uri(track_id)
+  def track_uri(track_id)
     "#{base_uri}/tracks/#{track_id}"
   end
 
-  def self.base_uri
+  def base_uri
     'http://localhost:3001'
   end
 end
